e2e4cc81bdde78a861bb2082c8f97c9a193c5a59
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2021-12-26 20:52:46 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//SINGLE BST NODE STRUCT
 struct node {
 	int data;
 	node* left;
@@ -13,6 +14,7 @@ struct node {
   }
 };
 
+//CREAT/ADD BST NODE FUNCTION
 node* addBST(node* root, int val) {
 	if (root==NULL) {
 		return new node(val);
@@ -26,6 +28,7 @@ node* addBST(node* root, int val) {
 	return root;
 }
 
+//FUNCTION FOR INORDER TRAVERSAL
 void Inorder(node* root) {
 	if (root==NULL) {
         return;
@@ -35,6 +38,7 @@ void Inorder(node* root) {
     Inorder(root->right);
 }
 
+//DRIVER CODE
 int main() {
 	node* root=NULL;
 	root=addBST(root, 1);
